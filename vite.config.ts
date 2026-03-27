8aba10683ca43eec5a39294d4472757d704d3477
am-consultingai <avishay@amconsultingai.com>
2026-03-27 05:51:35 +0300

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 export default defineConfig({
-  base: '/perion-ai-forall/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
